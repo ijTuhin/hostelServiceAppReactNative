d36fec46d8a25f412389372ee9ff43c3dc0e3fc4
ijTuhin <isratishu0209@gmail.com>
2023-08-01 02:51:18 +0600

    Login Screen redesigned

--- Components/Screens/LoginScreen.js
+++ Components/Screens/LoginScreen.js
@@ -4,24 +4,34 @@ import { StyleSheet } from "react-native";
 import LoginForm from "../Authentication/LoginForm";
 
 const LoginScreen = ({ navigation }) => {
-  const logo = require("../../assets/IIUClogo.jpg");
+  const logo = require("../../assets/app-icon.png");
   return (
     <ScrollView contentContainerStyle={styles.body}>
       <View style={styles.top}>
-        <Text style={{ fontSize: 32, color: "#d4d4d8" }}>
-          Welcome, Sign in to your Account
-        </Text>
+        <Image
+          style={{
+            width: 100,
+            height: 100,
+            resizeMode: "cover",
+          }}
+          source={logo}
+        />
         <Text
           style={{
             fontSize: 16,
             fontStyle: "italic",
             marginVertical: 4,
-            color: "#14b8a6",
+            color: "#0f766e",
+            width:60
           }}
         >
           Female Hostel Service
         </Text>
       </View>
+      <Text style={{ fontSize: 32, 
+    marginBottom: 12, color: "#d4d4d8" }}>
+        Welcome, Sign in to your Account
+      </Text>
       <LoginForm navigation={navigation} />
     </ScrollView>
   );
@@ -36,14 +46,13 @@ const styles = StyleSheet.create({
     alignItems: "center",
     paddingHorizontal: 32,
     width: "100%",
-    // marginTop: 33.6,
   },
   top: {
     display: "flex",
-    flexDirection: "column",
-    alignItems: "flex-start",
+    flexDirection: "row",
+    alignItems: "flex-end",
     justifyContent: "center",
-    marginBottom: 12,
+    marginBottom: 16,
   },
 });
 export default LoginScreen;
